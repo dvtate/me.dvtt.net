1d2d6dcd6f39e7b19acbe1d9383e3154886c185a
DV Tate Testa <toast27@gmail.com>
2020-09-14 17:29:48 -0500

    add lain

---
+++ lain/main.js
@@ -0,0 +1,60 @@
+// State vars
+let camera, scene, renderer, sprite, ring;
+
+// Setup
+function init() {
+
+	// Camera
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	// Scene
+	scene = new THREE.Scene();
+
+	// Lain
+	const loader = new THREE.TextureLoader();
+	sprite = new THREE.Sprite(new THREE.SpriteMaterial({
+		map: loader.load('http://me.dvtate.com/imgs/standing_single.png'),
+		alphaTest: 0.05,
+		transparent: false,
+	}));
+	scene.add( sprite );
+	
+	// Ring mesh
+	const ringTexture = loader.load('http://me.dvtate.com/imgs/middle_ring_tex.png');
+	ringTexture.wrapT = THREE.RepeatWrapping;
+	ring = new THREE.Mesh(
+		new THREE.CylinderGeometry(.3, .3, .02, 20, 32, true),
+		new THREE.MeshBasicMaterial({
+			map: ringTexture,
+			side: THREE.DoubleSide,
+			opacity: 0.5,
+			transparent: true,
+		}),	
+	);
+	ring.material.repea
+	ring.rotation.x += 0.2;
+	scene.add( ring );
+
+	// Renderer
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+	
+}
+
+// Animation loop
+function animate() {
+
+	    requestAnimationFrame( animate );
+	    
+	    ring.rotation.y += 0.05;
+
+	    renderer.render( scene, camera );
+
+}
+
+// Run app
+init();
+animate();
+
